dbb338c54a3eb6068cae0df808c1e9719c237bcc
burn7 <lsdizm@naver.com>
2021-10-26 23:20:20 +0000

    init

---
+++ Controllers/PublicDataConnectController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MangerBox.Controllers
+{
+    [ApiController]
+    [Route("public-data")]
+    public class PublicDataConnectController : ControllerBase
+    {
+        public PublicDataConnectController()
+        {
+            //   
+        }
+
+        [HttpGet]
+        public dynamic Get()
+        {
+            return new
+            {
+                TypeName = "public-data",
+                DateTime = DateTime.Now,                
+            };
+        }
+
+
+        // private static readonly string[] Summaries = new[]
+        // {
+        //     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        // };
+
+        // private readonly ILogger<WeatherForecastController> _logger;
+
+        // public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        // {
+        //     _logger = logger;
+        // }
+
+        
+    }
+}
